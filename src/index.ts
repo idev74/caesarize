8ee5a698904c03dc52b4c2b774601a394f6e29c3
Isabella Devassy <idevassy77@gmail.com>
2023-09-28 19:46:49 -0700

    linted

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,47 @@
 class Caesarize {
-    alphabet: string;
-    capitalAlphabet: string;
-    result: string;
+  alphabet: string
+  capitalAlphabet: string
+  result: string
 
-    constructor() {
-        this.alphabet = 'abcdefghijklmnopqrstuvwxyz';
-        this.capitalAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        this.result = '';
-    }
+  constructor () {
+    this.alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    this.capitalAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    this.result = ''
+  }
 
-    isCapital(letter: string): boolean {
-        return letter === letter.toUpperCase();
-    }
+  isCapital (letter: string): boolean {
+    return letter === letter.toUpperCase()
+  }
 
-    caseCheck(isCapital: boolean): string {
-        return isCapital ? this.capitalAlphabet : this.alphabet;
-    }
+  caseCheck (isCapital: boolean): string {
+    return isCapital ? this.capitalAlphabet : this.alphabet
+  }
 
-    shiftChar(char: string, shift: number, isEncrypt: boolean): string {
-        const isCharCapital = this.isCapital(char);
-        const letterCase = this.caseCheck(isCharCapital);
-        const index = letterCase.indexOf(char);
-        
-        return index === -1
-            ? char
-            : letterCase[(index + (isEncrypt ? shift : -shift) + letterCase.length) % letterCase.length];
-    }
-        
-    encrypt(text: string, shift: number): string {
-        this.result = '';
-        for (let i = 0; i < text.length; i++) {
-            this.result += this.shiftChar(text[i], shift, true);
-        }
-        return this.result;
-    }
+  shiftChar (char: string, shift: number, isEncrypt: boolean): string {
+    const isCharCapital = this.isCapital(char)
+    const letterCase = this.caseCheck(isCharCapital)
+    const index = letterCase.indexOf(char)
 
-    decrypt(text: string, shift: number): string {
-        this.result = '';
-        for (let i = 0; i < text.length; i++) {
-            this.result += this.shiftChar(text[i], shift, false);
-        }
-        return this.result;
+    return index === -1
+      ? char
+      : letterCase[(index + (isEncrypt ? shift : -shift) + letterCase.length) % letterCase.length]
+  }
+
+  encrypt (text: string, shift: number): string {
+    this.result = ''
+    for (let i = 0; i < text.length; i++) {
+      this.result += this.shiftChar(text[i], shift, true)
     }
+    return this.result
+  }
+
+  decrypt (text: string, shift: number): string {
+    this.result = ''
+    for (let i = 0; i < text.length; i++) {
+      this.result += this.shiftChar(text[i], shift, false)
+    }
+    return this.result
+  }
 }
 
-export { Caesarize };
+export { Caesarize }
